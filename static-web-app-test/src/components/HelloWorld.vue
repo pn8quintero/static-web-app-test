80ed629f572ba7f4fbc8f60c763ca5c67cf4c82e
jose-quintero-ob <jose.quintero@one-beyond.com>
2025-03-10 18:11:18 +0100

    Started

---
+++ static-web-app-test/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+const htmlContent = import.meta.env.VITE_HTML_CONTENT || "";
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div v-html="htmlContent"></div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
